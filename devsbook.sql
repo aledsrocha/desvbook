f8edb0802f46bf78dd1bf2b0db4a245fe27d64f4
Alessandro Rocha <58019461+aledsrocha@users.noreply.github.com>
2023-06-14 18:26:17 -0300

    Add files via upload
    db projeto

---
+++ devsbook.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 14-Jun-2023 às 23:25
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `devsbook`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `postcoments`
+--
+-- Erro ao ler a estrutura para a tabela devsbook.postcoments: #1932 - Table 'devsbook.postcoments' doesn't exist in engine
+-- Erro ao ler dados para tabela devsbook.postcoments: #1064 - Você tem um erro de sintaxe no seu SQL próximo a 'FROM `devsbook`.`postcoments`' na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `postlikes`
+--
+-- Erro ao ler a estrutura para a tabela devsbook.postlikes: #1932 - Table 'devsbook.postlikes' doesn't exist in engine
+-- Erro ao ler dados para tabela devsbook.postlikes: #1064 - Você tem um erro de sintaxe no seu SQL próximo a 'FROM `devsbook`.`postlikes`' na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `posts`
+--
+-- Erro ao ler a estrutura para a tabela devsbook.posts: #1932 - Table 'devsbook.posts' doesn't exist in engine
+-- Erro ao ler dados para tabela devsbook.posts: #1064 - Você tem um erro de sintaxe no seu SQL próximo a 'FROM `devsbook`.`posts`' na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `userrelations`
+--
+-- Erro ao ler a estrutura para a tabela devsbook.userrelations: #1932 - Table 'devsbook.userrelations' doesn't exist in engine
+-- Erro ao ler dados para tabela devsbook.userrelations: #1064 - Você tem um erro de sintaxe no seu SQL próximo a 'FROM `devsbook`.`userrelations`' na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users`
+--
+-- Erro ao ler a estrutura para a tabela devsbook.users: #1932 - Table 'devsbook.users' doesn't exist in engine
+-- Erro ao ler dados para tabela devsbook.users: #1064 - Você tem um erro de sintaxe no seu SQL próximo a 'FROM `devsbook`.`users`' na linha 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
